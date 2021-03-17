8bec50dd7b22f7c46c4e7522c338c384e5ba0c14
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-17 15:36:32 -0500

    commit tree bd06583becb1b01daf222a94c30b9aeebf48cb8f

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetchImages();
+})
+
+function fetchImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+        .then(response => response.json())
+        .then(results => {
+        results.message.forEach(image => addImageToDOM(image))
+    });
+}
+
+function addImageToDOM(image) {
+    console.log(image)
+    let container = document.querySelector('#dog-image-container')
+    let img = document.createElement('img')
+    img.src = image
+    console.log(img)
+    container.appendChild(img)
+};
